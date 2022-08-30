dbf3fc238756a60c028a59a76d8fb97a46373ded
Amogh Raut <110584519+Amonghus@users.noreply.github.com>
2022-08-30 11:43:06 +0530

    Add files via upload

---
+++ TealViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TealViewController.swift
+//  uinavcontrol
+//
+//  Created by Amogh Raut on 30/08/22.
+//
+
+import UIKit
+
+class TealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Teal Screen"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
